d0022d9ab7d76c43e88e51016e60f28616d5860c
emi3all <emi3all@hotmail.com>
2018-07-16 13:07:21 +0700

    update text

--- webhooks.php
+++ webhooks.php
@@ -18,14 +18,13 @@ if (!is_null($events['events'])) {
 		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
 			// Get text sent
 			$text = $event['source']['userId'];
-			$text = 'test';
 			// Get replyToken
 			$replyToken = $event['replyToken'];
 
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
-				'text' => $text
+				'text' => $text." => test naja"
 			];
 
 			// Make a POST Request to Messaging API to reply to sender

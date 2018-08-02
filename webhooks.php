137c943ad3ff5dc04e062b06131021c7a5c80c75
emi3all <emi3all@hotmail.com>
2018-08-02 17:52:52 +0700

    add header

--- webhooks.php
+++ webhooks.php
@@ -142,11 +142,14 @@ function postDataApi($url,$userId){
   
     ]
 }';
+
+	$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
 	$ch = curl_init( $url );
 	curl_setopt( $ch, CURLOPT_POST, 1);
 	curl_setopt( $ch, CURLOPT_POSTFIELDS, $myvars);
 	curl_setopt( $ch, CURLOPT_FOLLOWLOCATION, 1);
 	curl_setopt( $ch, CURLOPT_HEADER, 0);
+	curl_setopt( $ch, CURLOPT_HTTPHEADER, $headers);
 	curl_setopt( $ch, CURLOPT_RETURNTRANSFER, 1);
 	$response = curl_exec( $ch );
 	return $response;
